ab10ee1b8e869cab12a23273ff70b091cda782a2
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-08 11:31:03 +0000

    datatype in js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict";   // treat all js code as newer version
+
+// alert("Warning")     it's give popup on browser (but not used in node js)
+
+// use should also focus on code redability
+
+let name="Rahul"
+let age=22
+let isLoggedIn=true
+let price=undefined
+let cash=null
+
+console.table([name,age,isLoggedIn,price,cash])
+
+console.log(typeof null)
+console.log(typeof isLoggedIn)
