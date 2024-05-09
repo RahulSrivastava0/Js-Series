a5067a71087bb6a1c431328342c243fbb5b0cded
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-09 09:55:40 +0000

    conversion in js

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,15 @@
+let score="33"
+let score2="33abx"
+let score3=undefined
+
+console.log(typeof(score));
+
+let ans=Number(score);
+let ans2=Number(score2);
+let ans3=Boolean(score2);
+let ans4=Number(score3);
+
+console.log(ans);
+console.log(ans2);
+console.log(ans3);
+console.log(ans4);
