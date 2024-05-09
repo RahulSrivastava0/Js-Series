302e8897dc823164b79000ec2ca57e32623874f2
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-09 11:27:06 +0000

    comparision in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+/*
+console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+console.log(2<1)
+*/
+
+/*
+console.log("2">1)
+console.log("2">=1)
+console.log("2"==2)
+console.log("2"===1)
+*/
+// avoid these type of comparision
+/*
+console.log(null==0)
+console.log(null>0)
+console.log(null<0)
+console.log(null<=0)
+console.log(null>=0)
+*/
+
