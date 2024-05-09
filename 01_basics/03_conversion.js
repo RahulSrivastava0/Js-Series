dbb42e2fde7bd9703fce2c51f9cbad05bb158861
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-09 10:42:19 +0000

    added  operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,15 +1,35 @@
-let score="33"
-let score2="33abx"
-let score3=undefined
+// let score="33"
+// let score2="33abx"
+// let score3=undefined
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let ans=Number(score);
-let ans2=Number(score2);
-let ans3=Boolean(score2);
-let ans4=Number(score3);
+// let ans=Number(score);
+// let ans2=Number(score2);
+// let ans3=Boolean(score2);
+// let ans4=Number(score3);
 
-console.log(ans);
-console.log(ans2);
-console.log(ans3);
-console.log(ans4);
+// console.log(ans);
+// console.log(ans2);
+// console.log(ans3);
+// console.log(ans4);
+
+
+/*  operation */
+
+let value=3
+let negValue=-value;
+
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-1)
+// console.log(2*2)
+// console.log(6/2)
+// console.log(2**3)
+// console.log(9%4)
+
+let str1="hello"
+let str2=" Rahul"
+let ans=str1+str2
+console.log(ans)
