8bcdd53a8fcb6f57eff5c6d8b03e62a33549ddf5
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-12 09:43:38 +0000

    nums and maths in js

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,20 @@
+const score=45
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+
+const value = 45.7845
+console.log(value.toPrecision(2))
+
+const payCheck=1000000
+console.log(payCheck.toLocaleString('en-IN'))
+
+
+
+//  *********** Maths in Js ************
+
