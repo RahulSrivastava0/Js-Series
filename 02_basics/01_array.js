b617cb7298a6a1678097dd4128c0bf8e781f8d8c
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-13 12:00:17 +0000

    Array added

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+// Arrays
+/*
+const arr = [1,5,4,6,7]
+console.log(arr[2])
+
+const heroes = ['Ironman','spiderman','superman']
+console.log(heroes[1])
+
+const arr2 = new Array(1,3,5,7,9)
+console.log(arr2[4])
+*/
+
+//  Array method
+
+const arr = [1,3,4,8,7]
+arr.push(12)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.unshift(45)
+console.log(arr)
+
+arr.shift()
+console.log(arr)
