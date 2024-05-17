698cc7231b9b5c0557800114f30c0811061159d1
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-17 11:08:34 +0000

    slice and splice added

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -12,6 +12,7 @@ console.log(arr2[4])
 
 //  Array method
 
+/*
 const arr = [1,3,4,8,7]
 arr.push(12)
 console.log(arr)
@@ -24,3 +25,19 @@ console.log(arr)
 
 arr.shift()
 console.log(arr)
+
+
+const newArr = arr.join()
+console.log(newArr)
+console.log(arr)
+
+*/
+
+const arr=[1,2,3,4,5,6]
+const myn1 = arr.slice(1,3)
+console.log(myn1)
+console.log(arr)
+
+const myn2=arr.splice(1,4)
+console.log(arr)
+console.log(myn2)
