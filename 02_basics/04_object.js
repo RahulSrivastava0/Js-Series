29e55626efe3099b6e378c673a1ca8850586f4e1
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-18 09:51:53 +0000

    object part 2 is added

---
+++ 02_basics/04_object.js
@@ -0,0 +1,51 @@
+const tinderUser = {}
+
+tinderUser.id="123abc"
+tinderUser.Name="Rahul"
+tinderUser.age=21
+
+// console.log(tinderUser)
+
+const user = {
+    email:"hello@gmail.com",
+    age:21,
+    fullname:{
+        username:{
+            firstname:"Rahul",
+            lastname:"Srivastava"
+        }
+    }
+}
+
+console.log(user.fullname.username.firstname)
+
+
+//  merging 2 object in new object
+ 
+const obj1 ={
+    1:"a",
+    2:"b"
+}
+
+const obj2={
+    3:"c",
+    4:"d"
+}
+
+// it copy the properties and value from source and stored in the  target
+//const obj3 = Object.assign({},obj1,obj2)   
+
+
+// spread method
+const obj3 = {...obj1,...obj2}
+
+console.log(obj3)
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+
+console.log(tinderUser.hasOwnProperty('id'))
