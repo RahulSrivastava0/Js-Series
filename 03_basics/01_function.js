ba7d092a0bde60f67eb65168a9acea9e5b3265b1
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-05-25 15:12:16 +0000

    functions more topics

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -31,3 +31,38 @@ function isLoggedIn(username='rahul')
 }
 
 console.log(isLoggedIn('Vegeta'));
+
+
+// create a function to calculate cartPrice
+
+function cartPrice(val1,val2,...num1)
+{
+    return num1
+}
+
+console.log(cartPrice(200,300,400,5000,400,200))
+
+// passing object using function
+
+function newUser(anyObject)
+{
+    return `the username is ${anyObject.username} and the price is ${anyObject.price}`
+}
+
+console.log(newUser(
+    {
+        username:"Rahul",
+        price:400
+    }
+));
+
+// passing array in function
+
+const newArray = [10,20,30,40,50]
+
+function getArray(newArray)
+{
+    return newArray[2];
+}
+
+console.log(getArray(newArray));
