bdb8c39c00d68d48dff36b3ead7b69ff12718465
Rahul Kumar Srivastava <121481595+RahulSrivastava0@users.noreply.github.com>
2024-06-11 09:37:10 +0000

    while and do-while loop added

---
+++ 05_loops/02.js
@@ -0,0 +1,21 @@
+let i=1;
+while(i<=10)
+    {
+        console.log(i);
+        i++;
+    }
+let start=0;
+let myArr=['Superman','Thor','Spiderman']
+while(start<myArr.length)
+    {
+        console.log(myArr[start]);
+        start++;
+    }
+
+
+    // do-while loop
+let score=8
+do{
+    console.log(score);
+    score=score+1;
+}while(score<10);
